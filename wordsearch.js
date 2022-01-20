515e0ce9a9b3e7dae7e270f80b57360817fae41f
Chris Tate <chris_tate_94@hotmail.com>
2022-01-20 01:14:50 +0000

    used transpose function from matrix assignment

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+let transpose = require("./transpose");
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  let transposeLetters = transpose(letters);
+  const verticalJoin = transposeLetters.map((ls) => ls.join(""));
+
+  if (letters.length === 0) {
+    return false;
+  }
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (v of verticalJoin) {
+    if (v.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
